21f74aeaaffc15e5a9d8d84c4feb420d501fc81a
retro <retro@retros-mbp.home>
2023-03-16 07:50:52 +0000

    add social links

--- components/project.js
+++ components/project.js
@@ -1,7 +1,17 @@
 import Image from 'next/image';
 import styles from './project.module.scss';
 import { marked } from 'marked';
-import  React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
+import { SocialIcon } from 'react-social-icons';
+
+// component for each social media icon/link
+const SocialMediaIcon = ({ url }) => {
+    return (
+        <div className={styles.socialmediaicon}>
+            <SocialIcon url={url} style={{ height: 30, width: 30 }} />
+        </div>
+    )
+}
 
 export default function Project({ project }) {
     const descContainerRef = useRef();
@@ -13,8 +23,14 @@ export default function Project({ project }) {
     });
 
     return (
-        <div className={styles.project} onClick={() => window.open(project.url)}>
+        <div className={styles.project}>
             <div><h1 className={styles.title}>{project.name}</h1></div>
+            <div className={styles.linkgrid}>{project.links.map((link, i) => {
+                return (
+                    <SocialMediaIcon key={i} url={link} />
+                )
+            })}</div>
+            <div className={styles.projectbody} onClick={() => window.open(project.url)}>
             <div><Image className={styles.img}
                 priority
                 src={project.image}
@@ -30,6 +46,7 @@ export default function Project({ project }) {
                     )
                 })}
             </div>
+        </div>
         </div >
     )
 }
